a1749d1008b68315759377f480734ce78f616704
eric.biscondi@gmail.com <cendrine@cendrine-laptop.(none)>
2013-12-29 16:19:47 +0100

    RF connection working with Serial

--- emonTx_teleinfo/emonTx_teleinfo.ino
+++ emonTx_teleinfo/emonTx_teleinfo.ino
@@ -13,7 +13,7 @@
 */
 #include <Regexp.h>
 #include <SoftwareSerial.h>
-
+#define DEBUG (1)
 /* Example of data transmitted:
 ADCO 524563565245 /
 OPTARIF HC.. <
@@ -49,7 +49,9 @@ typedef struct {
 PayloadTX emontx;                                                       
 
 
-#define FILTERSETTLETIME 5000              //  Time (ms) to allow the filters to settle before sending data
+//#define FILTERSETTLETIME 5000              //  Time (ms) to allow the filters to settle before sending data
+
+#define FILTERSETTLETIME 5000             //  Time (ms) to allow the filters to settle before sending data
 
 #define freq RF12_433MHZ                   // Frequency of RF12B module can be RF12_433MHZ, RF12_868MHZ or RF12_915MHZ. You should use the one matching the module you have.
 const int nodeID = 10;                     // emonTx RFM12B node ID
@@ -62,38 +64,44 @@ const int UNO = 1;                         // Set to 0 if your not using the UNO
 ISR(WDT_vect) { Sleepy::watchdogEvent(); } // Attached JeeLib sleep function to Atmega328 watchdog - enables MCU to be put into sleep mode inbetween readings to reduce power consumption 
 
 #include "EmonLib.h"
-EnergyMonitor ct1,ct2,ct3;                                              // Create  instances for each CT channel
+EnergyMonitor ct1;                                              // Create  instances for each CT channel
 
 const int LEDpin = 9;                                                   // On-board emonTx LED 
 
 boolean settled = false;
-SoftwareSerial cptSerial(5, 6);
+SoftwareSerial mySerialOne(5, 6);
+SoftwareSerial mySerialTwo(8, 9); // Dummy connection to be able to disable mySerialOne
 
 char receivedChar ='\0';
-char frame[512] ="PAPP 02680 1\0";
-/* ADCO 524563565245 /\
-  OPTARIF HC.. < \
-  ISOUSC 20 8 \
-  HCHC 001065963 _\
-  HCHP 001521211 '\
-  PTEC HC.. S\
-  IINST1 001 I\
-  IINST2 001 I\
-  IINST3 001 I\
-  IMAX1 008 2 \
-  IMAX2 008 2 \
-  IMAX3 008 2 \
-  PMAX 06030 3\
-  PAPP 01250 +\
-  HHPHC E 0\
-  MOTDETAT 000000 B\
-  PPOT 00 #\
-  ADCO 524563565245 /\
-  OPTARIF HC.. <\
-  ISOUSC 20 8";
-*/
+//char frame[256] ="";
+//char frame[256] ="HCHC 008784338 /\0";
+//char frame[256] ="PAPP 01820 ,\0";
+#if 1
+char frame[256] ="ADCO 041130079749 D \
+OPTARIF HC.. < \
+ISOUSC 20 8 \
+HCHC 008784338 / \
+HCHP 014186978 ? \
+PTEC HP..   \
+  IINST1 001 I \
+IINST2 003 L \
+IINST3 004 N \
+IMAX1 021 3 \
+IMAX2 031 5 \
+IMAX3 030 5 \
+PMAX 12780 8  \
+PAPP 01880 2 \
+HHPHC A , \
+MOTDETAT 000000 B \
+PPOT 00 #";
+#endif
 int i = 0;
 
+int freeRam () {
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
 
 void send_rf_data()
 {
@@ -103,7 +111,10 @@ void send_rf_data()
   rf12_sendStart(0, &emontx, sizeof emontx);
   // set the sync mode to 2 if the fuses are still the Arduino default
   // mode 3 (full powerdown) can only be used with 258 CK startup fuses
+  
   rf12_sendWait(2);
+  //rf12_sendWait(1);
+  
   rf12_sleep(RF12_SLEEP);
 }
 
@@ -116,17 +127,22 @@ void emontx_sleep(int seconds) {
   // } else Sleepy::loseSomeTime(seconds*1000);
 }
 
-int findInMatch(MatchState* ms, char* label)
+int findInMatch(MatchState* ms, char* label, char* value)
 {
-char value [10] = ""; 
+char regex [24] = "";
 char checksum[8] = "";
 unsigned char sum = 0;		// Somme des codes ASCII du message + un espace
-unsigned char i = 0;		
- 
-  ms->Match ("(%a+).(%d+).(%d+)");
-  ms->GetCapture(label, 0);
-  ms->GetCapture(value, 1);
-  ms->GetCapture(checksum, 2);
+unsigned char i = 0;
+  strcpy(regex, label);		
+  strcat(regex,  ".(%w+).(%w+)");
+#if DEBUG  
+  Serial.println(regex);
+#endif
+  //ms->Match ("(%a+).(%d+).(%d+)");
+  ms->Match (regex);
+  //ms->GetCapture(label, 0);
+  ms->GetCapture(value, 0);
+  ms->GetCapture(checksum, 1);
 #if DEBUG  
   Serial.print(label);Serial.print(":"); Serial.println(value);
   Serial.print("CKSUM:"); Serial.println(checksum);
@@ -168,196 +184,125 @@ unsigned char i = 0;
   Serial.print("Computed checksum: ");Serial.println(sum, HEX);
   Serial.print("Provided checksum: ");Serial.println(atoi(checksum), HEX);
 #endif
-  if(sum == checksum[0])
+  if(sum == checksum[0]) {
+    Serial.print("Found correct "); Serial.print(label);Serial.print(" with value ");Serial.println(value);
     return 1;
+  }  
   else
     return 0;
 }
 
-int checkSumCheck(char *label, char *value, char checksum) 
-{
-unsigned char sum = 32 ;		// Somme des codes ASCII du message + un espace
-int i ;
- 
-  for (i=0; i < strlen(label); i++) sum = sum + label[i] ;
-  for (i=0; i < strlen(value); i++) sum = sum + value[i] ;
-  sum = (sum & 63) + 32 ;
-  Serial.print(label);Serial.print(" ");
-  Serial.print(value);Serial.print(" ");
-  Serial.println(checksum);
-  Serial.print("Sum = "); Serial.println(sum);
-  Serial.print("Cheksum = "); Serial.println(int(checksum));
-  if ( sum == checksum) return 1 ;	// Return 1 si checkum ok.
-  return 0 ;
-}
 
-
-#if 0
-/*called for each match */
-void match_callback  (const char * match,          // matching string (not null-terminated)
-                      const unsigned int length,   // length of matching string
-                      const MatchState & ms)      // MatchState in use (to get captures)
-{
-#if 0
-  char cap [10];   // must be large enough to hold captures
-  
-  Serial.print ("Matched: ");
-  Serial.write ((byte *) match, length);
-  Serial.println ();
-  
-  for (byte i = 0; i < ms.level; i++)
-    {
-    Serial.print ("Capture "); 
-    Serial.print (i, DEC);
-    Serial.print (" = ");
-    ms.GetCapture (cap, i);
-    Serial.println (cap); 
-    Serial.println (atol(cap), DEC);
-    }  // end of for each capture
-#endif
-}  // end of match_callback 
-#endif
 void setup ()
 {
   Serial.begin (9600);
-  cptSerial.begin(1200);
+  Serial.println ("Reset");
+//  Serial.print ("Free Mem: ");Serial.print(freeRam(), DEC);Serial.println (" B");
 
-  Serial.println ();
-
+  mySerialOne.begin(1200);  
+  mySerialTwo.begin(1200);
+//  Serial.print ("Free Mem: ");Serial.print (freeRam());Serial.println (" B");
+  
+  
   rf12_initialize(nodeID, freq, networkGroup);    // initialize RFM12B
   rf12_sleep(RF12_SLEEP);
 
   pinMode(LEDpin, OUTPUT);                        // Setup indicator LED
   digitalWrite(LEDpin, HIGH);
   
-  if (UNO) wdt_enable(WDTO_8S);                   // Enable anti crash (restart) watchdog if UNO bootloader is selected. Watchdog does not work with duemilanove bootloader                                                             // Restarts emonTx if sketch hangs for more than 8s
-
-}  // end of setup  
-
-void loop () {
-#if 1
-unsigned long count;
-char label[8] = "";
-char value [10] = ""; 
-char checksum[8] = "";
-unsigned char sum = 0;		// Somme des codes ASCII du message + un espace
-  /* Monitor the baterry of emontx */
-  emontx.battery = ct1.readVcc();                                      //read emonTx battey (supply voltage)
-  Serial.print(" "); Serial.print(emontx.battery);
-  Serial.println(); delay(100);
-
-#if 0
-  /* Read a teleinfo frame */
-  while(receivedChar != 0x02) // Wait for the beginning of the frame
-  {
-    if (cptSerial.available())
-   //   Serial.write(cptSerial.read() & 0x7F);
-      receivedChar = cptSerial.read() & 0x7F;   
-  }
-  i = 0;
-  while(receivedChar != 0x03) // 
-  {
-    if (cptSerial.available()) {
-   //   Serial.write(cptSerial.read() & 0x7F);
-      receivedChar = cptSerial.read() & 0x7F;
-      frame[i++] = receivedChar;
-      if( i > 512-1)
-      {
-        receivedChar=0x03;
-        i=0;
-      }
-      
-    }  
-  }
-  frame[i++]='\0';
-#endif
-
-  /* Match state object */
-  MatchState ms;
-  
-  /* string we are searching in */
-  ms.Target (frame);
-// original buffer
-  //Serial.println (frame);
+//  if (UNO) wdt_enable(WDTO_8S);                   // Enable anti crash (restart) watchdog if UNO bootloader is selected. Watchdog does not work with duemilanove bootloader                                                     
+  // Restarts emonTx if sketch hangs for more than 8s
 
   /* Init the TxPayload Structure */
   emontx.hchc = 0;
   emontx.hchp = 0;
   emontx.imax1 = emontx.imax2= emontx.imax3 = 0;
   emontx.iinst1 = emontx.iinst2= emontx.iinst3 = 0;
-  emontx.papp = emontx.papp = 0;
+  emontx.papp = emontx.pmax = 0;
+
+}  // end of setup  
+
+void loop () {
+char value [32] = ""; 
+#if TEST_SERIAL
+  if (mySerialOne.available())
+      Serial.write(mySerialOne.read() & 0x7F);
+}
+#else
+  /* Monitor the baterry of emontx */
+  emontx.battery = ct1.readVcc();                                      //read emonTx battey (supply voltage)
+  //Serial.print(" "); Serial.print(emontx.battery);
+  //Serial.println(); 
+  delay(100);
+
+  Serial.print ("Free Mem: ");Serial.print (freeRam());Serial.println (" B");
+  
+#if 0
+  /* Read a teleinfo frame */
+  mySerialOne.listen();
+
+  while(receivedChar != 0x02) // Wait for the beginning of the frame
+  {
+    if (mySerialOne.available())
+      receivedChar = mySerialOne.read() & 0x7F;
+//       Serial.write(receivedChar);
+         
+  }
+  i = 0;
+  while(receivedChar != 0x03) // 
+  {
+    if (mySerialOne.available()) {
+   //   Serial.write(cptSerial.read() & 0x7F);
+      receivedChar = mySerialOne.read() & 0x7F;
+  //     Serial.write(receivedChar);
+      frame[i++] = receivedChar;
+      if( i > 256-1)
+      {
+        receivedChar=0x03;
+        i=0;
+      }    
+    }  
+  }
+  frame[i++]='\0';
+  mySerialTwo.listen();
+#endif
+
+  /* Match state object */
+  MatchState ms;
+  
+    /* string we are searching in */
+  ms.Target (frame);
+// original buffer
+  Serial.println (frame);
+
+  if (findInMatch(&ms, "ADCO",value))  emontx.iinst1 = atol(value);      
         
-  if (findInMatch(&ms, "HCHC"))
-  {
-    Serial.println("Found correct HCHC value");
-    emontx.hchc =  (unsigned int)(atol(value)/1000);      
-  }
-
+  if (findInMatch(&ms, "HCHC",  value))  emontx.hchc =   (unsigned int)(atol(value)/1000);      
+  if (findInMatch(&ms, "HCHP",  value))  emontx.hchp =   (unsigned int)(atol(value)/1000); 
+  if (findInMatch(&ms, "IINST1",value))  emontx.iinst1 = atol(value);      
+  if (findInMatch(&ms, "IINST2",value))  emontx.iinst2 = atol(value);      
+  if (findInMatch(&ms, "IINST3",value))  emontx.iinst3 = atol(value);      
+  if (findInMatch(&ms, "IMAX1", value))  emontx.imax1 =  atol(value);      
+  if (findInMatch(&ms, "IMAX2", value))  emontx.imax2 =  atol(value);      
+  if (findInMatch(&ms, "IMAX3", value))  emontx.imax3 =  atol(value);      
+  if (findInMatch(&ms, "PMAX",  value))  emontx.pmax =   atol(value);      
+  if (findInMatch(&ms, "PAPP",  value))  emontx.papp =   atol(value);      
   
-  if (findInMatch(&ms, "HCHP"))
-  {
-    Serial.println("Found correct HCHP value");
-    emontx.hchp =  (unsigned int)(atol(value)/1000); 
-  }
-
-  if (findInMatch(&ms, "IINST1"))
-  {
-    Serial.println("Found correct IINST1 value");
-    emontx.iinst1 = atol(value);      
-  }
-
-  if (findInMatch(&ms, "IINST2"))
-  {
-    Serial.println("Found correct IINST2 value");
-    emontx.iinst2 = atol(value);      
-  }
-    
-  if (findInMatch(&ms, "IINST3"))
-  {
-    Serial.println("Found correct IINST3 value");
-    emontx.iinst3 = atol(value);      
-  }
-  
-  if (findInMatch(&ms, "IMAX1"))
-  {
-    Serial.println("Found correct IMAX1 value");
-    emontx.imax1 = atol(value);      
-  }
-
-  if (findInMatch(&ms, "IMAX2"))
-  {
-    Serial.println("Found correct IMAX2 value");
-    emontx.imax2 = atol(value);      
-  }
-    
-  if (findInMatch(&ms, "IMAX3"))
-  {
-    Serial.println("Found correct IMAX3 value");
-    emontx.imax3 = atol(value);      
-  }
-
-  if (findInMatch(&ms, "PMAX"))
-  {
-    Serial.println("Found correct PMAX value");
-    emontx.pmax = atol(value);      
-  }
-  
-  if (findInMatch(&ms, "PAPP"))
-  {
-    Serial.println("Found correct PAPP value");
-    emontx.papp = atol(value);      
-  }
-
   // because millis() returns to zero after 50 days ! 
+//  Serial.println(millis());
   if (!settled && millis() > FILTERSETTLETIME) settled = true;
 
   if (settled)                                                            // send data only after filters have settled
   { 
+#if 1 //DEBUG
     Serial.println("Sending data");
+#endif
     send_rf_data();                                                       // *SEND RF DATA* - see emontx_lib
+#if 1 //DEBUG
+    Serial.println("Data sent");
+#endif
     digitalWrite(LEDpin, HIGH); delay(2); digitalWrite(LEDpin, LOW);      // flash LED
     emontx_sleep(5);                                                      // sleep or delay in seconds - see emontx_lib
   }
-#endif
 }
-
+#endif
